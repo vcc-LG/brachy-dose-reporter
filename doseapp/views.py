1afaa65acbb13deaa8c4a5de99f71a69d0d72ac7
vcc-LG <leogarcia61@gmail.com>
2017-02-14 22:42:41 +0000

    added list patient view

--- doseapp/views.py
+++ doseapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Patient, Fraction
+
 
 # Create your views here.
 def patient_list(request):
-    return render(request, 'doseapp/patient_list.html', {})
+    patients = Patient.objects.all()
+    return render(request, 'doseapp/patient_list.html',{'patients':patients})
