79d17fc183954950e718450e087aa97bca63a785
vcc-LG <leogarcia61@gmail.com>
2017-02-17 11:39:24 +0000

    fixed patient list page

--- doseapp/urls.py
+++ doseapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^patient/new/$', views.patient_new, name='patient_new'),
     url(r'^patient/list/$', views.patient_list, name='patient_list'),
-    url(r'^patient/(?P<pk>\d+)/$', views.patient_detail, name='patient_detail'),
+    url(r'^patient/(?P<pk>V\d+)/$', views.patient_detail, name='patient_detail'),
     ]
