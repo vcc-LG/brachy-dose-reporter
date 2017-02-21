8cf8bba0964264b26bfc595b42e989a632c71ca6
vcc-LG <leogarcia61@gmail.com>
2017-02-21 10:20:07 +0000

    missed out the db settings

--- brachysite/settings.py
+++ brachysite/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
