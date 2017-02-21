67d6bb3c7597b489ebd5837c6848bd7d26c31fb3
vcc-LG <leogarcia61@gmail.com>
2017-02-21 13:33:32 +0000

    started adding tests

--- doseapp/tests.py
+++ doseapp/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from doseapp.models import Patient, Fraction
 
 class IndexViewTestCase(TestCase):
     def test_index(self):
@@ -13,7 +13,13 @@ class ListViewTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
 
 
-class ListViewTestCase(TestCase):
+class PatientDetailTestCase(TestCase):
     def test_index(self):
-        resp = self.client.get('/patient_list/')
-        self.assertEqual(resp.status_code, 200)
+        test_patient_id = 'V666666'
+        test_patient = Patient.objects.create(
+            patient_id=test_patient_id,
+            first_name='django',
+            last_name='test')
+        Patient.objects.filter(pk=test_patient.pk).update(first_name='dingo')
+        test_patient.refresh_from_db()
+        self.assertEqual(test_patient.first_name, 'dingo')
